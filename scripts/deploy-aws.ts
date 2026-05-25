dbd829da1da13ce6397aadfafdc2958625363b63
bluewuxi <ricky.yu@outlook.com>
2026-05-25 22:52:20 +1200

    Fix web deploy env file location
    Refs #28

--- scripts/deploy-aws.ts
+++ scripts/deploy-aws.ts
@@ -215,7 +215,7 @@ function deployWeb(parameters: DeploymentParameters): void {
 }
 
 function writeTemporaryWebDeployEnv(environmentName: EnvironmentName, env: NodeJS.ProcessEnv): string {
-  const temporaryEnvPath = `.env.${environmentName}.local`;
+  const temporaryEnvPath = join("apps", "web", `.env.${environmentName}.local`);
   writeFileSync(
     temporaryEnvPath,
     [
